85038d055160fb2acc54a4c523b00fbaa5cd52cf
johnhooft <110422043+johnhooft@users.noreply.github.com>
2023-05-02 09:43:11 -0700

    enable mesh convex script

---
+++ Assets/Scripts/EnableMesh.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EnableMesh : MonoBehaviour
+{
+    void Start()
+    {
+        // Iterate through all child objects
+        foreach (Transform child in transform)
+        {
+            // Get the Mesh Filter component
+            MeshFilter meshFilter = child.GetComponent<MeshFilter>();
+            if (meshFilter != null)
+            {
+                // Get the Mesh Collider component and enable it
+                MeshCollider meshCollider = child.GetComponent<MeshCollider>();
+                if (meshCollider == null)
+                {
+                    meshCollider = child.gameObject.AddComponent<MeshCollider>();
+                }
+                // Set the Mesh Collider to be convex
+                meshCollider.convex = true;
+                // Assign the mesh to the Mesh Collider
+                meshCollider.sharedMesh = meshFilter.sharedMesh;
+            }
+        }
+    }
+}
