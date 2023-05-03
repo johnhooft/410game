30e20ea2f66c2788f5d97b63e7c73c3ef62e11a0
colesimonse <126532660+colesimonse@users.noreply.github.com>
2023-05-03 00:21:42 -0700

    Revert "camera done"

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -7,11 +7,14 @@ Description:
 This script deals with the camera that follows the character.
 The camera currently maintains the the same direction all the time while the character rotates freely.
 */
-  //public Transform targetTransform; //object camera will follow.
-    //private Vector3 cameraFollowVelocity = Vector3.zero;
-    //public float cameraFollowSpeed = 0.2f;
 
-    /*private void Awake()
+public class CameraManager : MonoBehaviour
+{
+    public Transform targetTransform; //object camera will follow.
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+    public float cameraFollowSpeed = 0.2f;
+
+    private void Awake()
     {
         targetTransform = FindAnyObjectByType<PlayerManager>().transform;
     }
@@ -24,35 +27,4 @@ The camera currently maintains the the same direction all the time while the cha
 
 
     }
-
-    */
-public class CameraManager : MonoBehaviour
-{
-    
-        
-
-
-    
-
-    
-    public Transform camTarget;
-    
-    public float pLerp = .02f;
-    public float rLerp = .01f;
-    // Start is called before the first frame update
-   
-    
-
-    // Update is called once per frame
-    
-    
-    void Update()
-    {
-        transform.position = Vector3.Lerp(transform.position,camTarget.position,pLerp);
-        transform.rotation  = Quaternion.Lerp(transform.rotation,camTarget.rotation,rLerp);
-
-
-    }
-
-
 }
