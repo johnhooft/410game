5fea452ea67e084693ad7a65f05b3d8b83fddea4
Jack Cannon IV <JackIV@JC4s-New-Mac.local>
2023-05-13 11:44:12 -0700

    Dog Treat PickUp + Collectible Rotator Script

---
+++ Assets/Scripts/PickUpTreatRotator.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpTreatRotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update() { transform.Rotate(new Vector3(0, 60, 0) * Time.deltaTime); }
+}
