da9d652311c1143c52ad59e9bc893f582916240d
cole simonse <126532660+colesimonse@users.noreply.github.com>
2023-05-16 19:20:22 -0700

    respawn feature

--- Assets/Scripts/respawn.cs
+++ Assets/Scripts/respawn.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class respawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Transform player;
+    
     void Update()
     {
-        
+        if(player.position.y <= .5)
+        {
+            player.position = new Vector3( 0f, 1.85f,0f);
+
+
+        }
     }
 }
