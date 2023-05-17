affad6583b7d87cb73480ce55f72ee889243ee83
cole simonse <126532660+colesimonse@users.noreply.github.com>
2023-05-16 19:50:56 -0700

    npc dog updates

---
+++ Assets/Scripts/speech.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speech : MonoBehaviour
+{
+    
+    public GameObject text;
+    // Update is called once per frame
+    void Start()
+    {
+        text.SetActive(false);
+    }
+    void OnTriggerEnter(Collider player)
+    {
+        if(player.gameObject.tag == "Player")
+        {
+            text.SetActive(true);
+            StartCoroutine("WaitForSec");
+
+        }
+
+
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(10);
+        text.SetActive(false);
+
+
+    }
+}
