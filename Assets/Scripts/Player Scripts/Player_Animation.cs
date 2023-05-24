23833c2056191d5d3e9a68f3821a59b832cd05af
Leo Abrahamian <leo.abrahamian@outlook.com>
2023-05-23 18:44:34 -0700

    Refactoring code, strengthening Blend Tree-jumping.

--- Assets/Scripts/Player Scripts/Player_Animation.cs
+++ Assets/Scripts/Player Scripts/Player_Animation.cs
@@ -8,30 +8,33 @@ public class Player_Animation : MonoBehaviour
 {
 
     public Animator animator;
-    public InputManager inputManager;
+    InputManager inputManager;
+    PlayerLocomotion playerLocomotion;
+
     
     // Start is called before the first frame update
     void Awake()
     {
         animator = GetComponent<Animator>();
         inputManager =  transform.parent.GetComponent<InputManager>();
+        playerLocomotion = transform.parent.GetComponent<PlayerLocomotion>();
     }
 
     // Update is called once per frame
     public void HandleAnimation()
     {
-        bool isWalking = inputManager.horizontalInput != 0 || inputManager.verticalInput != 0;
-
-        if (isWalking)
-            animator.SetBool("IsWalking", isWalking);
-
-        /* Is the dog sprinting? */
-        animator.SetBool("IsSprinting", playerLocomotion.isSprinting)
         /* Essentially just */
         /** y = verticalInput; or y = movementInput.y; **/
         /** x = horizontalInput; or x = movementInput.x; **/
+        
         animator.SetFloat("x", verticalInput);
-        animator.SetFloat("y", horizontalInput)
-       
+        animator.SetFloat("y", horizontalInput);
+
+        bool isWalking = inputManager.horizontalInput != 0 || inputManager.verticalInput != 0;
+
+        animator.SetBool("IsWalking", isWalking);
+
+        /* Is the dog sprinting? (A component of InputManager */
+        animator.SetBool("IsSprinting", playerLocomotion.isSprinting); 
     }
 }
