44ba26a158a7291e0dfd0e9b9ee54b2ee22bedc4
johnhooft <110422043+johnhooft@users.noreply.github.com>
2023-05-30 16:00:54 -0700

    Level 3 teleport

---
+++ Assets/Scripts/Level 2/Level3Teleport.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level3Teleport : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {SceneManager.LoadScene(3);}
+    }
+}
